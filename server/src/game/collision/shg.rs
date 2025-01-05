56ed9ade312da4ebd79cb5a1cc1df575a1b3d2f4
Altanis <38045884+Altanis@users.noreply.github.com>
2025-01-05 17:52:21 -0500

    format shg for clarity

--- server/src/game/collision/shg.rs
+++ server/src/game/collision/shg.rs
@@ -8,69 +8,59 @@ struct Map(Vec<(u32, u32)>);
 
 /// An extremely optimized fixed-size hash table implementation.
 #[derive(Debug, Clone)]
-pub struct Table<T: Default + Clone>
-{
+pub struct Table<T: Default + Clone> {
     entries: Vec<T>,
     capacity: usize,
 }
 
-impl<T: Default + Clone> Table<T>
-{
+impl<T: Default + Clone> Table<T> {
     /// Create a new table with `size` entries.
-    pub fn new(size: usize) -> Self
-    {
+    pub fn new(size: usize) -> Self {
         let capacity = (size * 1000).next_power_of_two() + 1;
         let entries = vec![T::default(); capacity];
         Self { entries, capacity }
     }
 
     /// Get entry number.
-    pub fn count(&self) -> usize
-    {
+    pub fn count(&self) -> usize {
         self.entries.len()
     }
 
     #[inline(always)]
-    fn index(&self, idx: u64) -> usize
-    {
+    fn index(&self, idx: u64) -> usize {
         (hash_u64(idx) % self.entries.len() as u64) as usize
     }
 
     /// Get a mutable reference to an entry from a 2D key.
     #[inline(always)]
-    pub fn get_vector_mut(&mut self, x: u32, y: u32) -> &mut T
-    {
+    pub fn get_vector_mut(&mut self, x: u32, y: u32) -> &mut T {
         let idx = self.index(vector_hash(x, y));
         unsafe { self.entries.get_unchecked_mut(idx) }
     }
 
     /// Get a reference to an entry from a 2D key.
     #[inline(always)]
-    pub fn get_vector(&self, x: u32, y: u32) -> &T
-    {
+    pub fn get_vector(&self, x: u32, y: u32) -> &T {
         let idx = self.index(vector_hash(x, y));
         unsafe { self.entries.get_unchecked(idx) }
     }
 
     /// Get a reference to an entry from a scalar key.
     #[inline(always)]
-    pub fn get_scalar(&self, s: u32) -> &T
-    {
+    pub fn get_scalar(&self, s: u32) -> &T {
         let idx = self.index(hash_u64(s as u64));
         unsafe { self.entries.get_unchecked(idx) }
     }
 
     /// Get a mutable reference to an entry from a scalar key.
     #[inline(always)]
-    pub fn get_scalar_mut(&mut self, s: u32) -> &mut T
-    {
+    pub fn get_scalar_mut(&mut self, s: u32) -> &mut T {
         let idx = self.index(hash_u64(s as u64));
         unsafe { self.entries.get_unchecked_mut(idx) }
     }
 
     /// Clear the table.
-    pub fn clear(&mut self)
-    {
+    pub fn clear(&mut self) {
         self.entries.clear();
         self.entries.resize(self.capacity, T::default());
     }
@@ -78,18 +68,15 @@ impl<T: Default + Clone> Table<T>
 
 /// Spatial hash grid implementation.
 #[derive(Debug, Clone)]
-pub struct SpatialHashGrid
-{
+pub struct SpatialHashGrid {
     grid: Table<Entry>,
     maps: Table<Map>,
     shift: u32,
 }
 
-impl SpatialHashGrid
-{
+impl SpatialHashGrid {
     /// Create a new grid with a fixed bucket size and cell size.
-    pub fn new(size: usize, shift: u32) -> Self
-    {
+    pub fn new(size: usize, shift: u32) -> Self {
         Self {
             grid: Table::new(size),
             maps: Table::new(size),
@@ -98,14 +85,12 @@ impl SpatialHashGrid
     }
 
     /// Get size of internal tables.
-    pub fn count(&self) -> usize
-    {
+    pub fn count(&self) -> usize {
         self.grid.count()
     }
 
     /// Insert an entity.
-    pub fn insert(&mut self, id: u32, position: Vector2D<f32>, radius: f32)
-    {
+    pub fn insert(&mut self, id: u32, position: Vector2D<f32>, radius: f32) {
         let dimensions = radius * 2.0;
 
         let sx = (position.x as u32) >> self.shift;
@@ -126,8 +111,7 @@ impl SpatialHashGrid
     }
 
     /// Delete an entity by ID.
-    pub fn delete(&mut self, id: u32)
-    {
+    pub fn delete(&mut self, id: u32) {
         let map = self.maps.get_scalar(id);
         for &(x, y) in map.0.iter() {
             let cell = self.grid.get_vector_mut(x, y);
@@ -139,8 +123,7 @@ impl SpatialHashGrid
     }
 
     /// Retrieve entities in a circular region.
-    pub fn query_radius(&self, entity_id: u32, position: Vector2D<f32>, radius: f32) -> Vec<u32>
-    {
+    pub fn query_radius(&self, entity_id: u32, position: Vector2D<f32>, radius: f32) -> Vec<u32> {
         let mut result: Vec<u32> = Vec::new();
 
         let dimensions = radius * 2.0;
@@ -176,8 +159,13 @@ impl SpatialHashGrid
     }
 
     /// Retrieve entities in a rectangular region.
-    pub fn query_rect(&self, entity_id: u32, position: Vector2D<f32>, width: f32, height: f32) -> Vec<u32>
-    {
+    pub fn query_rect(
+        &self,
+        entity_id: u32,
+        position: Vector2D<f32>,
+        width: f32,
+        height: f32,
+    ) -> Vec<u32> {
         let mut result: Vec<u32> = Vec::new();
 
         let sx = (position.x as u32) >> self.shift;
@@ -211,29 +199,25 @@ impl SpatialHashGrid
     }
 
     /// Reinsert an entity into the grid.
-    pub fn reinsert(&mut self, id: u32, position: Vector2D<f32>, radius: f32)
-    {
+    pub fn reinsert(&mut self, id: u32, position: Vector2D<f32>, radius: f32) {
         self.delete(id);
         self.insert(id, position, radius)
     }
 
     /// Clear the grid.
-    pub fn clear(&mut self)
-    {
+    pub fn clear(&mut self) {
         self.grid.clear();
         self.maps.clear();
     }
 }
 
 #[inline]
-fn vector_hash(x: u32, y: u32) -> u64
-{
+fn vector_hash(x: u32, y: u32) -> u64 {
     ((x as u64) << 32) | y as u64
 }
 
 /// Identity hash for now
 #[inline]
-fn hash_u64(seed: u64) -> u64
-{
+fn hash_u64(seed: u64) -> u64 {
     seed
 }
